a612beeac74b9d2dfe1a844cc77cf1c20e371c4e
Jessie Douglas <jessielou.douglas@gmail.com>
2015-01-09 17:32:45 -0500

    Write CourseStudents and TeacherStudentLinks controller

--- app/controllers/course_students_controller.rb
+++ app/controllers/course_students_controller.rb
@@ -1,2 +1,52 @@
 class CourseStudentsController < ApplicationController
+  before_filter :require_logged_in, only: :create
+  before_filter :require_teacher_or_student, only: :destroy
+  
+  def create
+    if current_user.user_type == "Teacher"
+      course = Course.find(params[:course_students][:course_id])
+      unless current_user.taught_courses.include?(course)
+        flash[:errors] = ["Error. Course or student doesn't exist."]
+        redirect_to user_url(current_user)
+      end
+      
+      @link = CourseStudents.new(link_params)
+    elsif current_user.user_type == "Student"
+      course = Course.find_by_course_code(params[:course_code])
+      @link = course.links_with_students.new(student: current_user)
+    end
+    
+    unless @link.save
+      flash[:errors] = ["Error. Course or student doesn't exist."]
+    end
+    
+    redirect_to user_url(current_user)
+  end
+  
+  def destroy
+    CourseStudents.find(params[:id]).destroy
+    redirect_to user_url(current_user)
+  end
+  
+  private
+  def link_params
+    params.require(:course_students).permit(:course_id, :student_id)
+  end
+  
+  def require_teacher_or_student
+    link = CourseStudents.find(params[:id])
+    
+    if current_user.user_type == "Teacher"
+      user_courses = current_user.taught_courses
+      unless user_courses.include?(link.course)
+        flash[:errors] = ["Can't remove a student from another teacher's course."]
+        redirect_to user_url(current_user)
+      end
+    elsif current_user.user_type == "Student"
+      unless current_user.id == link.student_id
+        flash[:errors] = ["Can't remove another student from a course."]
+        redirect_to user_url(current_user)
+      end
+    end
+  end
 end
--- app/controllers/teacher_student_links_controller.rb
+++ app/controllers/teacher_student_links_controller.rb
@@ -1,9 +1,47 @@
 class TeacherStudentLinksController < ApplicationController
+  before_filter :require_logged_in, only: :create
+  before_filter :require_teacher_or_student, only: :destroy
+  
   def create
+    if current_user.user_type == "Teacher"
+      @link = current_user.links_with_students.new(link_params)
+    elsif current_user.user_type == "Student"
+      @link = current_user.links_with_teachers.new(link_params)
+    end
     
+    unless @link.save
+      flash[:errors] = ["Error. Student or teacher non-existant."]
+    end
+    
+    redirect_to user_url(current_user)
   end
   
   def destroy
     TeacherStudentLink.find(params[:id]).destroy
+    redirect_to user_url(current_user)
+  end
+  
+  private
+  def link_params
+    if current_user.user_type == "Teacher"
+      params.require(:teacher_student_link).permit(:student_id)
+    elsif current_user.user_type == "Student"
+      params.require(:teacher_student_link).permit(:teacher_id)
+    end
+  end
+  
+  def require_teacher_or_student
+    link = TeacherStudentLink.find(params[:id])
+    if current_user.user_type == "Teacher"
+      unless current_user.id == link.teacher_id
+        flash[:errors] = ["Can't remove a student from another teacher's list."]
+        redirect_to user_url(current_user)
+      end
+    elsif current_user.user_type == "Student"
+      unless current_user.id == link.student_id
+        flash[:errors] = ["Can't remove another student from a teacher's list."]
+        redirect_to user_url(current_user)
+      end
+    end
   end
 end
